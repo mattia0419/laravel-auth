ba86c23e10d2c8ce0c83ab41c5ed4024aa2eb5cd
mattia0419 <pironemattia6@gmail.com>
2023-10-24 01:22:43 +0200

    add destroy

--- resources/views/admin/posts/index.blade.php
+++ resources/views/admin/posts/index.blade.php
@@ -29,6 +29,9 @@
                     <td>
                         <a href="{{ route('admin.posts.show', $post) }}"><i class="fa-regular fa-eye"></i></a>
                         <a href="{{ route('admin.posts.edit', $post) }}"><i class="fa-solid fa-pencil"></i></a>
+                        <a data-bs-toggle="modal" data-bs-target="#delete-modal-{{ $post->id }}">
+                            <i class="fa-solid fa-trash fa-lg mt-3 text-danger"></i>              
+                        </a>                   
                     </td>
                 </tr>
             @empty
@@ -42,4 +45,28 @@
     {{ $posts->links('pagination::bootstrap-5') }}
 </div>
     
+@foreach($posts as $post)
+              <div class="modal fade" id="delete-modal-{{ $post->id }}" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+                <div class="modal-dialog">
+                  <div class="modal-content">
+                    <div class="modal-header">
+                      <h1 class="modal-title fs-5" id="exampleModalLabel">Elimina elemento</h1>
+                      <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                    </div>
+                    <div class="modal-body">
+                      Sei scuro di voler eliminare definitivamente?
+                    </div>
+                    <div class="modal-footer">
+                      <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Chiudi</button>
+                      <form action="{{ route('admin.posts.destroy', $post) }}" method="POST">
+                        @csrf
+                        @method('DELETE')
+                        <button class="btn btn-danger">Elimina</button>
+                      </form>
+                    </div>
+                  </div>
+                </div>
+              </div>
+@endforeach
+
 @endsection
